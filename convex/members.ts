add27c43a2ac07f7ef9ebff853198ffbfcd204ed
Harsh Sagar <harshsagar175@gmail.com>
2024-10-30 20:29:43 +0530

    started working to display all the channels and members a workspace have .

--- convex/members.ts
+++ convex/members.ts
@@ -1,24 +1,56 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
+import { query, QueryCtx } from "./_generated/server";
 import { auth } from "./auth";
+import { Id } from "./_generated/dataModel";
+
+
+const populateUser = (ctx: QueryCtx , userId: Id<"users">) => {
+
+};
+
+export const get = query({
+  args: { workspaceId: v.id("workspaces") },
+  handler: async ( ctx , args) => {
+    const userId = await auth.getUserId(ctx);
+    if (!userId) {
+      return [];
+    }
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) {
+      return [];
+    }
+    const data = await ctx.db
+        .query("members")
+        .withIndex("by_workspace_id" , (q) => q.eq("workspaceId" , args.workspaceId))
+        .collect();
+    
+    const members = [];
+  },
+});
 
 export const current = query({
-    args: { workspaceId: v.id("workspaces")} ,
-    handler: async (ctx , args) => {
-        const userId = await auth.getUserId(ctx);
-        if(!userId){
-            return null;
-        }
-        const member = await ctx.db
-            .query("members")
-            .withIndex("by_workspace_id_user_id" , (q) => 
-                q.eq("workspaceId" , args.workspaceId).eq("userId" , userId)
-            )
-            .unique();
-
-        if(!member) {
-            return null;
-        }
-        return member; 
+  args: { workspaceId: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+    if (!userId) {
+      return null;
     }
-})
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) {
+      return null;
+    }
+    return member;
+  },
+});
